282c8cdfa15cfe9a468f23053eaf0f522d1bb814
Sam Verschueren <sam.verschueren@gmail.com>
2016-03-20 13:02:38 +0100

    q → pinkie-promise

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var path = require('path');
 var fs = require('fs');
 var through = require('through2');
 var gutil = require('gulp-util');
-var Q = require('q');
+var Promise = require('pinkie-promise');
 var cordovaLib = require('cordova-lib').cordova;
 var cordova = cordovaLib.raw;
 
@@ -21,22 +21,29 @@ module.exports = function (options) {
 	}, function (cb) {
 		var exists = fs.existsSync(path.join(cordovaLib.findProjectRoot(), 'platforms', 'ios'));
 
-		Q.fcall(function () {
-			if (options.reAdd) {
-				// First remove the platform if we have to re-add it
-				return cordova.platforms('rm', 'ios');
-			}
-		}).then(function () {
-			if (exists === false || options.reAdd) {
-				// Add the iOS platform if it does not exist or we have to re-add it
-				return cordova.platforms('add', 'ios' + (options.version ? ('@' + options.version) : ''));
-			}
-		}).then(function () {
-			// Build the platform
-			return cordova.build({platforms: ['ios']});
-		}).then(cb).catch(function (err) {
-			// Return an error if something happened
-			cb(new gutil.PluginError('gulp-cordova-build-ios', err.message));
-		});
+		Promise.resolve()
+			.then(function () {
+				if (options.reAdd) {
+					// First remove the platform if we have to re-add it
+					return cordova.platforms('rm', 'ios');
+				}
+			})
+			.then(function () {
+				if (exists === false || options.reAdd) {
+					// Add the iOS platform if it does not exist or we have to re-add it
+					return cordova.platforms('add', 'ios' + (options.version ? ('@' + options.version) : ''));
+				}
+			})
+			.then(function () {
+				// Build the platform
+				return cordova.build({platforms: ['ios']});
+			})
+			.then(function () {
+				cb();
+			})
+			.catch(function (err) {
+				// Return an error if something happened
+				cb(new gutil.PluginError('gulp-cordova-build-ios', err.message));
+			});
 	});
 };
